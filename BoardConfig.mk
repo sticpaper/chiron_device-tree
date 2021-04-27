e2e24bcae17fe8730b21614dfa23f9b6d28f9464
stic-server-open <1138705738@qq.com>
2021-04-27 18:44:58 +0800

    chiron: Add the construction parameters of the paper

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,6 +34,25 @@ TARGET_RECOVERY_UI_MARGIN_STATUSBAR := 50
 # Display
 TARGET_SCREEN_DENSITY := 440
 
+# PaperROM Changes 添加更改部分 - 更新时间2021-04-03 下午22:39:42
+# 编译系统默认语言 (否则定义默认为en-US)
+PAPER_ROM_DEFAULT_LANGUAGES := zh-CN
+# 编译系统的地区类型 (类型为CN (中国版), GLOBAL (全球版), DEMO (拍视频演示版). )
+PAPER_ROM_BUILDSYSTEM_TYPE := CN
+# 编译系统时包括的语言包 (否则使用ROM源码默认参数 languages_all)
+PAPER_ROM_BUILDSYSTEM_LANGUAGES_LIST := languages_china
+# 编译系统使用的定义参数 (否则使用vendor_xxxxx的默认定义)
+PAPER_ROM_BUILDSYSTEM_MARK := vendor_paper_exthm
+# 编译系统是否启用外置软件包程序 (比如MIUI相机, GAPPS谷歌服务)
+PAPER_ROM_BUILDSYSTEM_OTHER_PACKAGES := false
+# 编译系统的版本维护类型 (否则则为unofficial)
+PAPER_ROM_BUILDSYSTEM_COMPILE_TYPE := UNOFFICIAL
+# 构建系统默认的版本 (分为用户版, 开发版, 测试版, 调试版)
+PAPER_ROM_BUILDSYSTEM_COMPILE_SYSTEM_TYPE := user
+# 是否禁用用户空间的垃圾模块 (比如无用的温控模块)
+PAPER_ROM_BLACKLIST_MAGISK_EXIT := true
+# PaperROM Changes 结束更改部分 - 更新时间2021-04-03 下午22:39:42
+
 # SELinux
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 
